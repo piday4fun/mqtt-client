a1e03603cc6a4a3169192c08decd4417ce48153a
yoyojacky <yoyojacky2009@gmail.com>
2020-03-14 02:19:29 +0800

    update mqtt_client.py
    Signed-off-by: yoyojacky <yoyojacky2009@gmail.com>

--- mqtt_client.py
+++ mqtt_client.py
@@ -9,14 +9,18 @@ def message(client, userdata, message):
 
 myClient = mqtt.Client("yoyojacky")
 myClient.connect("solasolo.oicp.net",1883)
-myClient.subscribe("Piday4fun")
+myClient.subscribe("piday4fun/action")
 myClient.on_message = message
 myClient.loop_start()
 
 try:
     while True:
-        temp = subprocess.getoutput('vcgencmd measure_temp') 
-        myClient.publish("Piday4fun", "{}".format(temp))
+        #temp = subprocess.getoutput('vcgencmd measure_temp') 
+        #myClient.publish("Piday4fun", "{}".format(temp))
+        if myClient.subscribe("piday4fun/action"):
+            subprocess.call(["/bin/stepper", "0", "1", "2", "3"])
+        else:
+            subprocess.call(["bin/stepper","3","2","1","0"])
         time.sleep(1)
 except KeyboardInterrupt:
     print("byebye!")
