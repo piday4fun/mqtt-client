c4a1012989d1d75d0568f1cea5d6151eac81a4c3
yoyojacky <yoyojacky2009@gmail.com>
2020-03-14 03:36:44 +0800

    update counting function
    Signed-off-by: yoyojacky <yoyojacky2009@gmail.com>

--- mqtt_client.py
+++ mqtt_client.py
@@ -1,27 +1,29 @@
 import paho.mqtt.client as mqtt
 import time
 import subprocess
+import cv2
+
+count = 0 
 
 def message(client, userdata, message):
+    global count 
     topic = str(message.topic)
     message = str(message.payload.decode("utf-8"))
     print(topic + message)
+    subprocess.call(["/bin/stepper", "0", "1", "2", "3"])
+    count +=1
+
 
 myClient = mqtt.Client("yoyojacky")
 myClient.connect("solasolo.oicp.net",1883)
-myClient.subscribe("piday4fun/action")
 myClient.on_message = message
-myClient.loop_start()
-
-try:
-    while True:
-        #temp = subprocess.getoutput('vcgencmd measure_temp') 
-        #myClient.publish("Piday4fun", "{}".format(temp))
-        if myClient.subscribe("piday4fun/action"):
-            subprocess.call(["/bin/stepper", "0", "1", "2", "3"])
-        else:
-            subprocess.call(["bin/stepper","3","2","1","0"])
-        time.sleep(1)
-except KeyboardInterrupt:
-    print("byebye!")
-
+myClient.subscribe("piday4fun/action", 1)
+# myClient.loop_forever()
+while True:
+    try:
+        myClient.loop(1)
+    except KeyboardInterrupt:
+        print(count)
+        for i in range(count):
+            subprocess.call(["/bin/stepper", "3", "2", "1", "0"])
+        break
